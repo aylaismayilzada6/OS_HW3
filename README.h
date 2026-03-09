2c5690b1e22f8fdb48afa550cda8a4bea7b6bcab
Ayla Ismayilzada <150772989+aylaismayilzada6@users.noreply.github.com>
2026-03-09 16:57:55 +0400

    Update README.h with program details and instructions
    Added detailed description of the Readers-Writers problem implementation, including compilation and execution instructions.

--- README.h
+++ README.h
@@ -1,1 +1,29 @@
-this Assignment 
+Readers-Writers Problem with Load Balancing
+
+Language:
+C using POSIX threads and semaphores
+
+Files included:
+- main.c
+- README.txt
+- report.txt
+
+Program description:
+This program implements the readers-writers synchronization problem with writer priority and load balancing across three file replicas.
+
+How to compile:
+gcc main.c -o program -lpthread
+
+How to run:
+./program
+
+Program behavior:
+- Multiple readers can read concurrently.
+- Readers are distributed across three replicas.
+- When the writer intends to write, new readers must wait.
+- The writer updates all replicas exclusively.
+- Each read/write operation is logged in log.txt.
+
+Output files:
+- log.txt (generated when the program runs)
+- the log.txt will be included in the zip file, but it is not required 
